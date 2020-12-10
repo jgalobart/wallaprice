502fdd380c86580d6853741f1c73ca97d5983fc2
Jordi Galobart <github@galu.cat>
2020-12-10 07:46:35 +0100

    results box

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -2,23 +2,45 @@ import './Results.css';
 
 function PriceBoxes (props) {
     var boxes = []
-    var i = 0;
+    var i;
     var step = (parseFloat(props.max)-parseFloat(props.min))/10;
     for (i=0;i<10;i++) {
         var start = i*step;
         var end = i*step+step;
-        boxes.push(<td>{start}€ - {end}€</td>);
+        boxes.push(<th>{start}€ - {end}€</th>);
+    }
+    return boxes;
+}
+
+function Distribution (props) {
+    var i;
+    var boxes = []
+    for (i=0;i<10;i++) {
+        var distribution = Math.floor(Math.random() * 100);
+        boxes.push(<td>{distribution}%</td>);
     }
     return boxes;
 }
 
 export default function Results () {
     return (
-        <table>
-            <tr>
-                <PriceBoxes min="0" max="1000"/>
-            </tr>
-        </table>
+        <>
+            <table>
+                <tfoot>
+                    <td colspan="10">
+                        Haz click en cada rango de precios para ver más detalle
+                        </td>
+                </tfoot>
+                <tbody>
+                    <tr>
+                        <PriceBoxes min="0" max="1000"/>
+                    </tr>
+                    <tr>
+                        <Distribution />
+                    </tr>
+                </tbody>
+            </table>
+        </>
     );
 }
 
