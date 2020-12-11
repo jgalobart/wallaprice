62f5dda9bd05d81f9f222df72d1fe02d6bb77c1d
Jordi Galobart <github@galu.cat>
2020-12-11 07:35:47 +0100

    parser gateway

--- api/getContent.py
+++ api/getContent.py
@@ -1,24 +1,18 @@
 from http.server import BaseHTTPRequestHandler
+from selenium import webdriver
 
-import urllib.request
+class handler(BaseHTTPRequestHandler):
 
-def do_GET(self,search=""):
-    try:
+    def do_GET(self,search="",latitude="40.43786",longitude="-3.81962",min_sale_price="50",max_sale_price="10000"):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        LATITUDE="40.43786"
-        LONGITUDE="-3.81962"
-        URL = "https://es.wallapop.com/search?keywords="+search+"&latitude="+LATITUDE+"&longitude="+LONGITUDE+"&filters_source=search_box"
+        URL = "https://es.wallapop.com/search?keywords="+search+"min_sale_price="+min_sale_price+"&max_sale_price="+max_sale_price+"&latitude="+latitude+"&longitude="+longitude+"&filters_source=search_box"
         print(URL)
-        webURL = urllib.request.urlopen(URL)
-        data = webURL.read()
-        self.wfile.write(data.encode())
-        return
-    catch():
-        self.send_response(500)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-
-data = getContent('test')
-print(data)
+        browser = webdriver.Firefox()
+        browser.get('http://seleniumhq.org/')
+        data = "test"
+        print(data)
+        self.wfile.write(data)
+        print("end")
+        return(data)
